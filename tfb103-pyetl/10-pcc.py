6aab38aeddbb5f64258f41e2c49fa35fe3d21ec1
uuboyscy <u.shi@lndata.com>
2021-08-07 11:17:23 +0800

    Update 20210807 pcc form data and hidden value

--- tfb103-pyetl/10-pcc.py
+++ tfb103-pyetl/10-pcc.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://web.pcc.gov.tw/tps/pss/tender.do?searchMode=common&searchType=advance'
+urlPost = 'https://web.pcc.gov.tw/tps/pss/tender.do?searchMode=common&searchType=advance'
+url = 'https://web.pcc.gov.tw/tps/pss/tender.do?method=goSearch&searchMode=common&searchType=advance&searchTarget=ATM'
 
 userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
 headers = {
@@ -44,4 +45,17 @@ for r in dataStr.split('\n'):
     data[key] = value
 
 data = {i.split(': ')[0]: i.split(': ')[1] for i in dataStr.split('\n')}
+# print(data)
+
+res = requests.post(url, headers=headers, data=data)
+# print(res.text)
+
+soup = BeautifulSoup(res.text, 'html.parser')
+hiddenInputs = soup.select('input[type="hidden"]')
+for hiddenInput in hiddenInputs:
+    print(hiddenInput)
+    try:
+        data[hiddenInput['name']] = hiddenInput['value']
+    except KeyError:
+        pass
 print(data)
